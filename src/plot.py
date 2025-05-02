0926e0edaff40524f67f03db30526bbd7100da16
DeepWater3104 <redcurry1106@gmail.com>
2025-05-02 18:10:09 +0900

    update

--- src/plot.py
+++ src/plot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from mpi4py import MPI
 
 def motion(event):
     x = [event.xdata]
@@ -7,8 +8,33 @@ def motion(event):
     ln.set_data(x,y)
     plt.draw()
 
-plt.figure()
-ln, = plt.plot([],[],'x')
+def plot(comm):
+    plt.figure()
+    ln, = plt.plot([],[],'x')
+    
+    plt.connect('motion_notify_event', motion)
+    plt.show()
 
-plt.connect('motion_notify_event', motion)
-plt.show()
+def calculate(comm):
+    print('aiueo')
+    # initialize neural network object
+
+    # initialize spike list
+
+    # the time resolution of spike shownin the figure equals to  the minimum time of syanptic delay
+
+    # wait for actions by a user  
+
+    
+
+
+if __name__ == '__main__':
+    # get rank
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+
+    if rank == 0:
+        plot(comm)
+    else:
+        calcualte(comm)
