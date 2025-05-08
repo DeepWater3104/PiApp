f98f00733a9df71e73ceef1d695b5d76bf160bbd
DeepWater3104 <redcurry1106@gmail.com>
2025-05-08 16:58:45 +0900

    update

---
+++ src/network_params.py
@@ -0,0 +1,8 @@
+num_neurons_percol = 20
+num_neurons_perrow = 50
+
+network_params = {
+    'num_neurons_percol': num_neurons_percol,
+    'num_neurons_perrow': num_neurons_perrow,
+    'num_neurons': num_neurons_percol * num_neurons_perrow,
+}
--- src/plot.py
+++ src/plot.py
@@ -1,38 +1,67 @@
 import numpy as np
-def motion(ln, ax, event):
+import math
+
+def update_heatmap(data, ax, params):
+    # modify to update only after allgathered
+    data = data[:params['num_neurons']].reshape((params['num_neurons_perrow'], params['num_neurons_percol']))
+    heatmap = ax.imshow(data, cmap='hot', interpolation='nearest', alpha=0.6) # I don't know parameters' meaning
+
+
+def gather_loop(comm, spikes_innode, ax, params, userinput):
+    while True:
+        time.sleep(0.5)
+        data = comm.allgather(spikes_innode)
+        data = np.hstack(data[1:])
+        update_heatmap(data, ax, params)
+        comm.Bcast(userinput, root=0)
+        #print(userinput)
+        userinput = np.zeros(params['num_neurons'])
+
+
+def motion(ln, ax, num_neurons_perrow, userinput, event):
     x = [event.xdata]
     y = [event.ydata]
 
-    heatmap_data = np.random.random((100, 100))
-    heatmap = ax.imshow(heatmap_data, cmap='hot', interpolation='nearest', alpha=0.6)
+    # insert to userinput array
+    if event.xdata != None and event.ydata != None:
+        print(int(math.floor(event.xdata))*num_neurons_perrow + int(math.floor(event.ydata)))
+        userinput[int(math.floor(event.xdata))*num_neurons_perrow + int(math.floor(event.ydata))] = 1
+        #print('debug:' + str(int(math.floor(event.xdata))) + " " + str(int(math.floor(event.ydata))) + " " + str(int(math.floor(event.xdata))*num_neurons_perrow + int(math.floor(event.ydata))))
 
     ln.set_data(x,y)
     plt.draw()
 
-#def neuralactivity(ln):
-#    #spikes = np.zeros((1000, 1000))
-#    ln.histgram2d(spikes)
 
-def plot(comm):
+def plot(comm, spikes_innode, params):
     plt.figure()
     fig, ax = plt.subplots()
     ln, = plt.plot([],[],'x', color='blue', markersize=10)
     
-    #plt.connect('motion_notify_event', motion)
-    plt.connect('motion_notify_event', partial(motion, ln, ax))
-    #neuralactivity(ln)
+    userinput = np.zeros(params['num_neurons'])
+    plt.connect('motion_notify_event', partial(motion, ln, ax, params['num_neurons_perrow'], userinput))
+    gather_thread = threading.Thread(target=gather_loop, args=(comm, spikes_innode, ax, params, userinput), daemon=True)
+    gather_thread.start()
     plt.show()
 
-#def calculate(comm):
-    # initialize neural network object
 
-    # initialize spike list
+def calculate(comm, spikes_innode, params):
+    time_index = 0
+    userinput = np.zeros(params['num_neurons'])
+    while True:
+        time_index += 1
+        #spikes_innode += 1
+        if time_index in range(params['num_neurons_offset'], min(params['num_neurons_offset']+params['num_neurons_each']-1, params['num_neurons'])):
+            spikes_innode[time_index] += 1
 
-    # the time resolution of spike shownin the figure equals to  the minimum time of syanptic delay
+        #for i in range(params['num_neurons_offset'], min(params['num_neurons_offset']+params['num_neurons_each']-1, params['num_neurons'])):
+        #    if userinput[i] > 0:
+        #        spikes_innode[i - params['num_neurons_offset']] = 1
+                #print('debug')
 
-    # wait for actions by a user  
-
-    
+        if time_index % 5 == 0:
+            data = comm.allgather(spikes_innode)
+            comm.Bcast(userinput, root=0)
+            #spikes_innode = np.zeros(network_params['num_neurons_each'])
 
 
 if __name__ == '__main__':
@@ -43,10 +72,16 @@ if __name__ == '__main__':
     rank = comm.Get_rank()
 
     import time
+    from network_params import network_params
+
+    network_params['num_neurons_each'] = int((network_params['num_neurons'] + (size - 1) - 1 ) / (size - 1))
+    network_params['num_neurons_offset'] = network_params['num_neurons_each'] * (rank - 1)
+    spikes_innode = np.zeros(network_params['num_neurons_each'])
 
     if rank == 0:
         import matplotlib.pyplot as plt
         from functools import partial
-        plot(comm)
-    #else:
-    #    calcualte(comm)
+        import threading
+        plot(comm, spikes_innode, network_params)
+    else:
+        calculate(comm, spikes_innode, network_params)
