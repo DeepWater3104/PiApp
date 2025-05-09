31c080276f17fb6e1bdae1e8d82a247a6c2a9de7
DeepWater3104 <redcurry1106@gmail.com>
2025-05-09 09:49:35 +0900

    update

--- src/HopfieldSNN.py
+++ src/HopfieldSNN.py
@@ -1,10 +1,66 @@
 import numpy as np
 
-class SNN():
-    # variables
+class snn():
+    def __init__(self, params):
+        self.params = params
 
-    def __init__(params):
+        # constants
+        self.dt = 0.2
+        self.tau = 20.
+        self.i_strength = 1.5
+        self.vrest = -65.
+        self.theta = -55.
+        self.r_m = 16.
+        self.tau_syn = 4.
+        self.ref = 1
 
-    def update_LIF():
+        # initialize synapse
+        self.syn_w     = np.zeros((params['num_neurons'], params['num_neurons_each']))
+        self.syn_delay = np.random.randn(params['num_neurons'], params['num_neurons_each']) + 1
+        self.syn_g     =  np.zeros(params['num_neurons_each'])
 
-    def calculate_synaptic_current():
+        # initialize stimulus
+        self.i_ext = np.zeros(params['num_neurons_each'])
+
+        # initialize membrane potential
+        self.v = np.zeros(params['num_neurons_each'])
+
+        # initialize refractory period
+        self.ref_left = np.zeros(params['num_neurons_each'])
+
+    def update_LIF(self, spikes_innode, userinput, t):
+        noise = (3 * np.random.randn(self.params['num_neurons_each']) + 1) * np.sqrt(self.dt)
+        self.i_ext = self.i_strength * userinput[self.params['num_neurons_offset']:self.params['num_neurons_offset']+self.params['num_neurons_each']]
+        self.v += ((self.vrest - self.v + self.syn_g + self.r_m * self.i_ext) * self.dt + noise) / self.tau
+
+        #for i in range(self.params['num_neurons_each']):
+        #    if self.theta < self.v[i]:
+        #        self.v[i] = self.vrest
+        #        spikes_innode[i] = t
+
+        #    if 0 < self.ref_left[i]:
+        #        self.ref_left[i] -= 1
+        #        self.v[i] = self.vrest
+
+        spiking = self.v > self.theta
+        self.v[spiking] = self.vrest
+        spikes_innode[spiking] = t
+
+        refractory = self.ref_left > 0
+        self.ref_left[refractory] -= 1
+        self.v[refractory] = self.vrest
+
+
+    def update_synapse(self, delay_left, t):
+        #print(np.shape(delay_left))
+        transmitted = (delay_left == 1)
+        transmitting = delay_left > 0
+        delay_left[transmitting] -= 1
+
+        #r = np.zeros(self.params['num_neurons_each'])
+        #for post in range(self.params['num_neurons_each']):
+        #    for pre in range(self.params['num_neurons']):
+        #        r[post] += self.w[pre, post] * transmitted[pre]
+        r = transmitted.astype(float) * self.syn_w
+        #print(np.shape(r))
+        self.syn_g = self.syn_g * np.exp(-self.dt/self.tau_syn) + np.sum(r, axis=0)
--- src/plot.py
+++ src/plot.py
@@ -1,12 +1,13 @@
 def update_heatmap(data, heatmap, params):
     data = data[:params['num_neurons']].reshape((params['num_neurons_perrow'], params['num_neurons_percol']))
+    data = data > 0
     heatmap.set_data(data)
     #plt.draw() # cannot draw from subthraed
 
 
 def gather_loop(comm, spikes_innode, heatmap, params, userinput):
     while True:
-        time.sleep(0.5)
+        time.sleep(0.1)
         data = comm.allgather(spikes_innode)
         data = np.hstack(data[1:])
         update_heatmap(data, heatmap, params)
@@ -41,25 +42,23 @@ def mstprocess(comm, spikes_innode, params):
 
 
 def slvprocess(comm, spikes_innode, params):
+    snn_each = snn(params)
     time_index = 0
     userinput = np.zeros(params['num_neurons'])
+    delay_left = np.zeros((params['num_neurons'], params['num_neurons_each']))
     while True:
         time_index += 1
-        #spikes_innode += 1
-        #if time_index in range(params['num_neurons_offset'], min(params['num_neurons_offset']+params['num_neurons_each']-1, params['num_neurons']-1)):
-        #    spikes_innode[time_index - params['num_neurons_offset']] += 1
-
-        
-        for i in range(params['num_neurons_offset'], min(params['num_neurons_offset']+params['num_neurons_each']-1, params['num_neurons']-1)):
-            if userinput[i] > 0:
-                spikes_innode[i-params['num_neurons_offset']] = 5
-            else:
-                spikes_innode[i-params['num_neurons_offset']] = 0
-
+        snn_each.update_LIF(spikes_innode, userinput, time_index) 
+        snn_each.update_synapse(delay_left, time_index)
+    
         if time_index % 5 == 0:
             data = comm.allgather(spikes_innode)
+            spike_times = np.hstack(data[1:])  # shape: (N,)
+            spike_matrix = np.tile(spike_times[:, np.newaxis], (1, params['num_neurons_each']))  # shape: (N, N_each)
+            delay_left = np.maximum(spike_matrix + snn_each.syn_delay - 5, 0)
             comm.Bcast(userinput, root=0)
-            #spikes_innode = np.zeros(network_params['num_neurons_each'])
+            time_index = 0
+            spikes_innode = np.zeros(params['num_neurons_each'])
 
 
 if __name__ == '__main__':
@@ -76,12 +75,15 @@ if __name__ == '__main__':
 
     network_params['num_neurons_each'] = int((network_params['num_neurons'] + (size - 1) - 1 ) / (size - 1))
     network_params['num_neurons_offset'] = int(network_params['num_neurons_each'] * (rank - 1))
-    spikes_innode = np.zeros(network_params['num_neurons_each'])
+    spikes_innode = np.zeros(network_params['num_neurons_each'], dtype=int)
 
     if rank == 0:
         import matplotlib.pyplot as plt
+        import matplotlib
+        #matplotlib.use('TkAgg')  # バックエンドをTkAggに変更
         from functools import partial
         import threading
         mstprocess(comm, spikes_innode, network_params)
     else:
+        from HopfieldSNN import snn
         slvprocess(comm, spikes_innode, network_params)
